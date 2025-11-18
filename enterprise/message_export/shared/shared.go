c9887d863bccf2b05eb76e1e2b00c9e02232451e
Tawan <arthur@avatolabs.com>
2025-11-18 16:26:49 +0800

    Add message_export/shared package for mmctl

---
+++ enterprise/message_export/shared/shared.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.enterprise for license information.
+
+package shared
+
+// Job data keys for message export
+const (
+	JobDataInitiatedBy     = "initiated_by"
+	JobDataExportType      = "export_type"
+	JobDataBatchStartId    = "batch_start_id"
+	JobDataJobStartId      = "job_start_id"
+	JobDataBatchStartTime  = "batch_start_time"
+	JobDataJobStartTime    = "job_start_time"
+	JobDataJobEndTime      = "job_end_time"
+	JobDataExportDir       = "export_dir"
+)
